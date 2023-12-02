ff8e0ef08a70b0226f61c76375622ff44639fa39
Devin Chenyang <devin.chenyang@grabtaxi.com>
2023-12-02 23:22:12 +0800

    syncup packages

--- packages/components/build.sh
+++ packages/components/build.sh
@@ -1,5 +1,5 @@
 # Cannot call `./build.sh` directly, otherwise you will see an error "tsc: command not found"
-# Instead, call `npm run build-lib` to run this script
+# Instead, call `npm run build` to run this script
 
 rm -rf dist
 ./node_modules/.bin/babel src --out-dir dist --extensions '.ts,.tsx,.js,.jsx,.snap' --copy-files --ignore 'src/**/*.js.snap,src/index.tsx' --no-copy-ignored --source-maps
