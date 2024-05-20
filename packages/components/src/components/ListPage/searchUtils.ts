d9c648decdf8727f19b21430b5fae9a7d893e1fc
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-05-20 22:08:32 +0800

    improve search

--- packages/components/src/components/ListPage/searchUtils.ts
+++ packages/components/src/components/ListPage/searchUtils.ts
@@ -1,78 +1,90 @@
 /**
- * Search "oo"(keyword) in "foobar200"(text)
+ * Search "oo" (keyword) in "foobar200" (text)
  * @param {string} keyword
  * @param {string} text
+ * @returns {boolean}
  */
-export const searchKeywordInText = (keyword: string, text: string) =>
-  text.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+export const searchKeywordInText = (keyword: string, text: string): boolean =>
+  text.toLowerCase().includes(keyword.toLowerCase());
 
 /**
- * Search "234"(keyword) in `123456`(text)
+ * Search "234" (keyword) in `123456` (text)
  * @param {string} keyword
  * @param {number} number
+ * @returns {boolean}
  */
-export const searchKeywordInNumberText = (keyword: string, number: number) =>
-  searchKeywordInText(keyword, String(number));
+export const searchKeywordInNumberText = (
+  keyword: string,
+  number: number
+): boolean => searchKeywordInText(keyword, String(number));
 
 /**
  * Search string 'do' in string array ['dog', 'cat']
- * @param {string|undefined} keyword Search keyword
- * @param {string[]} tags The table cell value
- * @return {boolean}
+ * @param {string|undefined} keyword - Search keyword
+ * @param {string[]} tags - The table cell value
+ * @returns {boolean}
  */
-export const searchStringInArray = (keyword: string, tags: string[]) =>
-  tags.reduce((prev, tag) => prev || searchKeywordInText(keyword, tag), false);
+export const searchStringInArray = (keyword: string, tags: string[]): boolean =>
+  tags.some((tag) => searchKeywordInText(keyword, tag));
 
 /**
  * Search exact keyword 'dog' NOT in string array ['dog', 'cat'] (Inverse exact match)
- * @param {string|undefined} keyword Search keyword
- * @param {string[]} tags The table cell value
- * @return {boolean}
+ * @param {string|undefined} keyword - Search keyword
+ * @param {string[]} tags - The table cell value
+ * @returns {boolean}
  */
-export const searchExactStringNotInArray = (keyword: string, tags: string[]) =>
-  tags.every((tag) => !searchKeywordInText(keyword, tag));
+export const searchExactStringNotInArray = (
+  keyword: string,
+  tags: string[]
+): boolean => tags.every((tag) => !searchKeywordInText(keyword, tag));
 
-// keywords="ap+ba" tags=["apple","banana"]
-// take 1st kw "ap": "apple".indexOf("ap")=>0, "banana".indexOf("ap")=>-1, data has tags matched "ap", => true
-// take 2rd kw "ba": "apple".indexOf("ba")=>-1, "banana".indexOf("ba")=>0, data has tags matched "ba", => true
-// true && true => true
+/**
+ * Search keywords in tags with logic AND
+ * @param {string[]} keywords - Search keywords
+ * @param {string[]} tags - The table cell value
+ * @returns {boolean}
+ */
 export const searchKeywordsInTagsWithLogicAnd = (
   keywords: string[],
   tags: string[]
-) => keywords.reduce((prev, kw) => prev && searchStringInArray(kw, tags), true);
+): boolean =>
+  keywords.reduce((prev, kw) => prev && searchStringInArray(kw, tags), true);
 
-// keywords="ap ba" data=["apple","pair"]
-// take 1st kw "ap": "apple".indexOf("ap")=>0, "pair".indexOf("ap")=>-1, data has tags matched "ap", => true
-// take 2rd kw "ba": "apple".indexOf("ba")=>-1, "pair".indexOf("ba")=>-1, data has no tags matched "ba", => false
-// true || false => true
+/**
+ * Search keywords in tags with logic OR
+ * @param {string[]} keywords - Search keywords
+ * @param {string[]} tags - The table cell value
+ * @returns {boolean}
+ */
 export const searchKeywordsInTagsWithLogicOr = (
   keywords: string[],
   tags: string[]
-) =>
+): boolean =>
   keywords.reduce((prev, kw) => prev || searchStringInArray(kw, tags), false);
 
 /**
  * Search filterKeyword in cellValue
- * @param {string} filterKeyword Search keyword
+ * @param {string} filterKeyword - Search keyword
  * - AND: "a+b" to search ["a","b"]
  * - OR : "a b" to search ["a"]
- * @param {string[]} [cellValue] The table cell value
+ * @param {string[]} [cellValue] - The table cell value
+ * @returns {boolean}
  */
 export const stringArrayFilter = (
   filterKeyword: string,
   cellValue: string[] = []
-) => {
+): boolean => {
   // first will try to parse operator from filterKeyword
   // choose different search logic based on operator
 
   // AND
-  if (filterKeyword.indexOf('+') !== -1) {
+  if (filterKeyword.includes('+')) {
     const keywords = filterKeyword.split('+');
     return searchKeywordsInTagsWithLogicAnd(keywords, cellValue);
   }
 
   // OR
-  if (filterKeyword.indexOf(' ') !== -1) {
+  if (filterKeyword.includes(' ')) {
     const keywords = filterKeyword.split(' ');
     return searchKeywordsInTagsWithLogicOr(keywords, cellValue);
   }
