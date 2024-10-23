7a57e4bcd0cf40032e99d0812d2b71d746b7c936
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-23 23:48:20 +0800

    Remove unnecessary React imports from NotFound, DbTablePageWrapper, and IframePageWrapper components

--- packages/components/src/layout/DbTablePageWrapper.tsx
+++ packages/components/src/layout/DbTablePageWrapper.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types, react/destructuring-assignment, max-len, no-console, react/no-unused-class-component-methods */
 
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { message } from 'antd';
 import { GithubDb } from '@db-man/github';
