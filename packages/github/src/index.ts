d4ba21044da1110281b607dbdd8c39be9cdbadda
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-24 23:27:58 +0800

    Refactor index.ts to streamline exports and include insights utilities

--- packages/github/src/index.ts
+++ packages/github/src/index.ts
@@ -1,7 +1,9 @@
-import GithubDb from './GithubDb';
-import Github from './Github';
 import * as types from './types';
 import * as contants from './constants';
+import GithubDb from './GithubDb';
+import Github from './Github';
+
+export { types, contants, GithubDb, Github };
 
 export * as utils from './utils';
-export { GithubDb, Github, types, contants };
+export * as insightsUtils from './insightsUtils';
