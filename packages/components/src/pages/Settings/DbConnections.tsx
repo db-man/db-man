d20883c60da2cd6fec4d2dd9de72bd98c404b5d9
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-27 23:03:05 +0800

    Remove unused Github import from DbConnections component

--- packages/components/src/pages/Settings/DbConnections.tsx
+++ packages/components/src/pages/Settings/DbConnections.tsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 
-import { Github } from '@db-man/github';
 import { Button, Tooltip, Typography } from 'antd';
 
 import * as constants from '../../constants';
