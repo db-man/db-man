1cf11603b1cdcaac1e6a331248525d19297d048b
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-27 22:26:47 +0800

    upgrade cypress from v12 to v13

--- packages/components/src/pages/Settings/SettingSwitch.cy.tsx
+++ packages/components/src/pages/Settings/SettingSwitch.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import SettingSwitch from './SettingSwitch'
+import React from 'react';
+import SettingSwitch from './SettingSwitch';
 
 describe('<SettingSwitch />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<SettingSwitch />)
-  })
-})
+    cy.mount(<SettingSwitch label='Test' storageKey='foo' />);
+  });
+});
