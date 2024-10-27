7ef097f02e7b2f2d5039b14ac1c56f32e5327046
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-10-27 22:36:27 +0800

    Refactor Cypress configuration for consistent string usage and remove experimental flag

--- packages/components/cypress.config.js
+++ packages/components/cypress.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   // The rest of the Cypress config options go here...
-  projectId: "mxr8b9",
+  projectId: 'mxr8b9',
 
   e2e: {
-    // cy.session() requires enabling the experimentalSessionAndOrigin flag.
-    experimentalSessionAndOrigin: true,
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -13,8 +11,8 @@ module.exports = {
 
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 };
