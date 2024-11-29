063b4ea4e5fd4c3e94db9d33bdb0b54edf7109b9
Devin Chenyang <devin.chenyang@grabtaxi.com>
2024-11-29 23:09:43 +0800

    refactor: remove unnecessary React import from PhotoList and UpdatePage components

--- packages/components/src/components/PhotoList.tsx
+++ packages/components/src/components/PhotoList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { List, Button, message } from 'antd';
 
 import { ImageLink } from './Links';
--- packages/components/src/pages/DbTablePage/UpdatePage/index.tsx
+++ packages/components/src/pages/DbTablePage/UpdatePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { utils as githubUtils, types } from '@db-man/github';
 import { message, Alert, Spin, Skeleton } from 'antd';
