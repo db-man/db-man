151289cedcea8ad044b00b8c766c3be3d324c58d
Devin Chenyang <devin.chenyang@grabtaxi.com>
2025-03-30 00:22:33 +0800

    move out EditableCell

--- packages/components/src/components/EditableTable/index.tsx
+++ packages/components/src/components/EditableTable/index.tsx
@@ -2,16 +2,9 @@
 
 import React, { useState } from 'react';
 
-import {
-  Table,
-  Input,
-  Popconfirm,
-  Form,
-  Typography,
-  Button,
-  Checkbox,
-  Select,
-} from 'antd';
+import { Table, Popconfirm, Form, Typography, Button } from 'antd';
+
+import EditableCell from './EditableCell';
 
 export type TableRowType = {
   [key: string]: string | boolean | undefined;
@@ -31,70 +24,6 @@ export type editableTableColumnType = {
 };
 
 export type isEditingType = (record: TableRowType) => boolean;
-
-function EditableCell({
-  editing,
-  dataIndex,
-  title,
-  record,
-  index,
-  children,
-  'ui:type': uiType,
-  'ui:options': uiOptions,
-  'form:required': formRequired,
-  'form:valueType': formValueType,
-  ...restProps
-}: {
-  editing: boolean;
-  dataIndex: string;
-  title: string;
-  record: any;
-  index: number;
-  children: any;
-  'ui:type': string;
-  'ui:options': { label: string; value: string }[];
-  'form:required': boolean;
-  'form:valueType': string;
-}) {
-  let input = (
-    <Input
-      className={`dbm-editable-table-new-row-editable-cell-title--${title}`}
-      placeholder=""
-    />
-  );
-  let valuePropName = undefined;
-  if (uiType === 'checkbox') {
-    input = <Checkbox />;
-    valuePropName = 'checked';
-  }
-  if (uiType === 'select') {
-    input = <Select options={uiOptions} />;
-    // valuePropName = 'value';
-  }
-  return (
-    <td {...restProps}>
-      {editing ? (
-        <Form.Item
-          name={dataIndex}
-          valuePropName={valuePropName}
-          style={{
-            margin: 0,
-          }}
-          rules={[
-            {
-              required: formRequired,
-              message: `Please Input ${title}!`,
-            },
-          ]}
-        >
-          {input}
-        </Form.Item>
-      ) : (
-        children
-      )}
-    </td>
-  );
-}
 
 function EditableTable({
   rowKey,
